9528347b964095126c23785afcd97c7c178f2d86
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-10-04 12:22:11 +0300

    creates a question page

---
+++ src/Components/Notes/question.js
@@ -0,0 +1,16 @@
+function Questions() {
+    return(
+       <div>
+          <form>
+          <textarea type="text">
+         </textarea>
+           </form> 
+           <div class="btn">
+            <button type="submit" class="btn1">Post</button>
+            <button class="btn2">Exit</button>
+        </div>
+
+       </div>
+    )
+   }
+   export default Questions;

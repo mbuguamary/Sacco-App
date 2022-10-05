dee266e467039f2fc726c9e104d9ebe0029ccf4f
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-10-05 23:38:11 +0300

    added hanle submit form

--- src/Components/Notes/question.js
+++ src/Components/Notes/question.js
@@ -1,13 +1,37 @@
+import { useState } from "react";
+
+
 function Questions() {
+   const [formData,setFormData] = useState({});
+   const [suggestion,setSuggestion] = useState({});
+
+   function handleChangeForm (e){
+    setFormData({...formData, [e.target.name]:[e.target.value]})
+   }
+    function handleSubmitForm(e){
+    e.preventDefault();
+    fetch("http://localhost:8001/suggestions",{
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json",
+          "accept":"application/json"
+       },
+       body: JSON.stringify(formData) 
+    })
+    .then(res => res.json())
+    .then((data) => {
+      setSuggestion([...suggestion,data])
+    })
+    }
     return(
        <div>
-          <form>
-          <textarea type="text">
-         </textarea>
+          <form onSubmit={handleSubmitForm}>
+          <textarea type="text" name={formData.suggestion} onChange={handleChangeForm}/>
+         
            </form> 
            <div class="btn">
             <button type="submit" class="btn1">Post</button>
-            <button class="btn2">Exit</button>
+            <button onClose>x</button>
         </div>
 
        </div>

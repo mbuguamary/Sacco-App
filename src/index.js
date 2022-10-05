dfeec336a83c5224b6f5f83a213ddd5c2d881527
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-10-05 15:47:01 +0300

    imported the components to index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
+import AddLoanForm from './Components/Loans/AddLoanForm';
+import LoanList from './Components/Loans/LoanList';
+import Notes from './Components/Notes/Notes'
+import Questions from './Components/Notes/question';
+import Login from './Components/Authentication/Login';
+
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<App/>}>
+            <Route path="/AddLoanForm" element={<AddLoanForm/>}/>
+            <Route path="/LoanList" element={<LoanList/>}/>
+            <Route path="/Notes" element={<Notes/>}/>
+            <Route path="/Questions" element={<Questions/>}/>
+            <Route path="/Login" element={<Login/>}/>
+        </Route>
+        
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 

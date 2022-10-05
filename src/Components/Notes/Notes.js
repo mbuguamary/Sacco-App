74d6a3b0b148175c894dbbd6a68c3e6a0c68d833
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-10-05 23:37:47 +0300

    corrected spelling

--- src/Components/Notes/Notes.js
+++ src/Components/Notes/Notes.js
@@ -1,9 +1,11 @@
+import './Notes.css'
+
 function Notes() {
  return(
     <div>
        <form>
        <textarea>
-1.Enter your ID No. to access your Sacco information. You will be able to check your Share Statement,Dividend Statement,Guarantorship,Loan Statement, Apply an intant Loan and post a question or a suggestion.
+1.Enter your ID No. to access your Sacco information. You will be able to check your Share Statement,Dividend Statement,Guarantorship,Loan Statement, Apply an instant Loan and post a question or a suggestion.
 2.If unable to access , Please liaise with Dan on 0785278786 or email: sacco@metro-hospital.com.
 3.Dividends applies to LAST Year's shares and NOT the current. 
 4.WHT only applies to those who will tick to be paid their dividends , it does NOT apply to all the other modes.

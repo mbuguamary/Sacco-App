b1233ee2e55864813bd4d60d3f32134b8dc3bc1d
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-10-07 17:11:43 +0300

    changed url

--- src/Components/Notes/question.js
+++ src/Components/Notes/question.js
@@ -1,38 +1,58 @@
+import { Button, Form } from "antd";
+import TextArea from "antd/lib/input/TextArea";
 import { useState } from "react";
 
 
-function Questions() {
+function Questions(props) {
+   const [form] = Form.useForm();
+   const suggestions = props.suggestions;
+  const setSuggestion = props.setSuggestion;
    const [formData,setFormData] = useState({});
-   const [suggestion,setSuggestion] = useState({});
+   //const [suggestion,setSuggestion] = useState({});
 
    function handleChangeForm (e){
     setFormData({...formData, [e.target.name]:[e.target.value]})
    }
     function handleSubmitForm(e){
-    e.preventDefault();
-    fetch("http://localhost:8001/suggestions",{
+   //  e.preventDefault();
+   
+    console.log(e);
+    fetch("http://localhost:8001/suggestions", {
       method: "POST",
       headers: {
-         "Content-Type": "application/json",
-          "accept":"application/json"
+         "Content-Type": "application/json"
+         
        },
-       body: JSON.stringify(formData) 
+       body: JSON.stringify(e) 
     })
     .then(res => res.json())
-    .then((data) => {
-      setSuggestion([...suggestion,data])
+    .then(data => {
+      //setSuggestion([...suggestions,data])
+      console.log("submitted")
+      form.resetFields();
     })
+    .catch(err => console.log(err.message))
     }
     return(
        <div>
-          <form onSubmit={handleSubmitForm}>
-          <textarea type="text" name={formData.suggestion} onChange={handleChangeForm}/>
+         <Form form={form} onFinish={handleSubmitForm} onSubmit={handleSubmitForm}>
+            <Form.Item name='suggestion'>
+               <TextArea></TextArea>
+            </Form.Item>
+            <Form.Item>
+               <Button type="primary" htmlType="submit">Submit</Button>
+            </Form.Item>
+         </Form>
+
+          {/* <form onSubmit={handleSubmitForm}>
+          <textarea type="text" name="suggestion" value={formData.suggestion} onChange={handleChangeForm}/>
          
-           </form> 
-           <div class="btn">
-            <button type="submit" class="btn1">Post</button>
-            <button onClose>x</button>
+          <div className="btn">
+            <button type="submit" className="btn1">Post</button>
+            <button >x</button>
         </div>
+           </form>  */}
+           
 
        </div>
     )

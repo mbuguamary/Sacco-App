1173b3c78a07a72e002d24b9f6fcd589c8929739
Mary Mbugua <mbuguamels@gmail.com>
2023-05-11 16:51:48 +0300

    created dividend report list

---
+++ src/Components/Reports/DividendList.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from 'react'
+import Dividends from "../Reports/Dividends";
+
+const DividendList = (member_no) => {
+const [dividends, setDividends] = useState([]);
+
+useEffect(() =>{
+fetch(`http://localhost:8080/api/v1/instant/${member_no}`)
+.then(res => res.json())
+.then(data => {setDividends(data);
+      
+    });
+
+},[])
+
+    
+
+  return (
+    <div>
+    <table>
+        <tbody>
+        <tr>
+                <th>Member Statement Summary</th>
+                </tr>
+            <tr>
+                <th>Name</th>
+                </tr>
+            <tr>
+                <th>member_no</th>
+                <th>tel</th>
+                </tr>
+                <tr>
+                <th>email</th>
+                <th>Id no</th>
+                </tr>
+                <tr>
+                <th>Print Date</th>
+                <th>Shares</th>
+                <th>Shares</th>
+                </tr>
+        </tbody>
+
+    </table>
+   
+    <table >
+    <tbody>
+      { <tr >
+        <th >
+          <h3 className="ui center aligned header">Loan No</h3>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Purpose</h3>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Sdate</h3>
+
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Edate</h3>
+          
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Period</h3>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Original Amount</h3>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Debit</h3>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Credits</h3>
+        </th>
+      </tr> }
+
+      {dividends.map(dividend => {
+        return (
+        <Dividends
+        key={dividend.id}
+        id={dividend.id}
+        loanno={dividend.loan_no}
+        purpose={dividend.purpose}
+        cdate={dividend.cdate}
+        sdate={dividend.sdate}
+        period={dividend.period}
+        amount={dividend.amount}
+        />
+      );
+        }
+      ) 
+    }
+    
+    
+    
+    </tbody>
+  </table>
+  </div>
+);
+  
+}
+
+export default DividendList
---
+++ src/Components/Reports/Dividends.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Dividends = () => {
+    let member_no;
+  return (
+    <div>Dividends</div>
+  )
+}
+
+export default Dividends
